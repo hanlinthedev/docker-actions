dd9f7e3428db6a50db898a0b5b15cf7f98b2d4ff
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2025-01-23 15:09:40 +0630

    update script

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,10 +11,10 @@ echo Giphy response - $giphy_response
 
 gif_url= $(echo "$giphy_response" | jq --raw-output .data.images.downsized.url)
 
-comment_response = $(curl -sX POST \
+comment_response= $(curl -sX POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
-  -d "{\"body\":\"### PR - #$pull_req_number\n ### Thank you for your contribution. \n ![Gif]($gif_url)\"}"
+  -d "{\"body\":\"### PR - #$pull_req_number\n ### Thank you for your contribution. \n ![Gif]($gif_url)\"}" \
   "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$pull_req_number/comments" )
 
 echo response - $comment_response
