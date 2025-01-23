9db47bbe5211333d1453372efcbfb3135c88d90a
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2025-01-23 15:13:51 +0630

    update script again

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,15 @@
 GITHUB_TOKEN=$1
 GIPHY_APIKEY=$2
 
-pull_req_number= $(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+pull_req_number=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
 echo PR_NUMBER - $pull_req_number
 
-giphy_response= $(curl -s "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_APIKEY&tag=&rating=g")
+giphy_response=$(curl -s "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_APIKEY&tag=&rating=g")
 echo Giphy response - $giphy_response
 
-gif_url= $(echo "$giphy_response" | jq --raw-output .data.images.downsized.url)
+gif_url=$(echo "$giphy_response" | jq --raw-output .data.images.downsized.url)
 
-comment_response= $(curl -sX POST \
+comment_response=$(curl -sX POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer $GITHUB_TOKEN" \
   -d "{\"body\":\"### PR - #$pull_req_number\n ### Thank you for your contribution. \n ![Gif]($gif_url)\"}" \
