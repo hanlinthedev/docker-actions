e7ae1b69a1a18d27f42f6dd12483e3dd799ea472
HanlinTheDEVELOPER <hanlinthedeveloper@gmail.com>
2025-01-23 14:27:36 +0630

    workflow

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Container image that runs your code
+FROM alpine:3.10
+
+RUN  apt update && \
+     apt install -y curl && \
+     apt install -y jq 
+
+# Copies your code file from your action repository to the filesystem path `/` of the container
+COPY entrypoint.sh /entrypoint.sh
+
+RUN chomd +x /entrypoint.sh
+
+# Code file to execute when the docker container starts up (`entrypoint.sh`)
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+GITHUB_TOKEN=$1
+GIPHY_APIKEY=$2
+
+pull_req_number= $(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+echo PR_NUMBER - $pull_req_number
+
+giphy_response= $(curl -s "https://api.giphy.com/v1/gifs/random?api_key=$GIPHY_APIKEY&tag=&rating=g")
+echo Giphy response - $giphy_response
+
+gif_url= $(echo "$giphy_response" | jq --raw-output .data.images.downsized.url)
+
+comment_response = $(curl -sX POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -d "{\"body\":\"### PR - #$pull_req_number\n ### Thank you for your contribution. \n ![Gif]($gif_url)\"}"
+  "https://api.github.com/repos/$GITHUB_REPOSITORY/issues/$pull_req_number/comments" )
+
+echo response - $comment_response
